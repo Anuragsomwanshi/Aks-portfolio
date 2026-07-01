0aa195ec372a4454b03e786621e1520db23bc488
Anuragsomwanshi <anuragsomwanshi11@gmail.com>
2026-07-01 13:46:15 +0530

    new change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
  }
   return (
     <>
-    <div className='flex  w-full  p-4 bg-yellow-200'>
+    <div className='flex flex-col md:flex-row   w-full  p-4 bg-yellow-200'>
         
         
 
@@ -24,29 +24,29 @@ const Navbar = () => {
             <span className='font-bold text-2xl'>KARTIK</span>
 
             <div  className={`
-    ${isOpen ? "flex flex-col items-center" : "hidden"}
+    ${isOpen ? "flex  justify-evenly ": "hidden "}
     md:flex md:flex-row
     w-full
     justify-end
     px-4 md:px-28
-    space-y-4 md:space-y-0 md:space-x-10
+      md:space-x-10
     transition-all duration-300
   `}>
 
                 {navMenu.map((item,index)=>(
-                    <a className='text-sm   md:text-2xl border rounded bg-white p-1 md:border-transparent hover:border-black transition duration-400' key={index} onClick={closeMenu}  href={`#${item}`}>{item}</a>
+                    <a className='text-sm md:text-2xl border rounded bg-white p-1 md:border-transparent md:bg-transparent hover:border-black transition duration-400' key={index} onClick={closeMenu}  href={`#${item}`}>{item}</a>
                     
                     
                 ))}
                 
 
+            </div>
 
 
    
 
-            </div>
         
-                <button onClick={show} className='px-2 py-2 border md:hidden absolute right-10  cursor-pointer'>=</button>
+                <button onClick={show} className='px-2  border md:hidden absolute right-5  cursor-pointer'>=</button>
 
 
 
